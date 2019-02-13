83abdfffa607115e539db366950bb81a3df8be18
James Shaker <james.shaker@gmail.com>
2019-02-13 16:03:08 +1100

    Added tactic selection cleanup for tactic sending and line/column number markers for general selection

--- text_transfer.py
+++ text_transfer.py
@@ -68,16 +68,66 @@ class HolReplSend(sublime_plugin.TextCommand):
 
 class HolReplTransferCurrent(sublime_plugin.TextCommand):
     def run(self, edit, scope="selection", action="send", prepend="", append=""):
-        text = ""
         if scope == "selection":
-            text = self.selected_text()
+            lN,cN,text = self.selected_text()
         elif scope == "lines":
-            text = self.selected_lines()
-        elif scope == "function":
-            text = self.selected_functions()
+            lN,cN,text = self.selected_lines()
         elif scope == "file":
-            text = self.selected_file()
-        text = prepend + text + append
+            lN,cN,text = self.selected_file()
+        else:
+            lN,cN,text = (0,0,"")
+        #Delete things off front dependent on type of selection sent
+        #TACTIC HANDLING
+        if prepend[-2:] == "e(":
+            #Get rid of leading whitespace and keep track
+            ntext = text.lstrip()
+            #Whilst we have leading tactic concatenators
+            while (ntext[0:2] in [r'>-',r'>|',r'>>',r'\\']) or (ntext[0:5] in ['THEN1','THENL', 'THEN ']):
+                #get rid of them
+                if ntext[0:2] in [r'>-',r'>|',r'>>',r'\\']:
+                    ntext = ntext[2:]
+                if ntext[0:5] in ['THEN1','THENL','THEN ']:
+                    ntext = ntext[5:]
+                #and any revealed whitespace
+                ntext = ntext.lstrip()
+            #Record deletion off front
+            dL = len(text) - len(ntext)
+            #Get rid of trailing whitespace
+            ntext = ntext.rstrip()
+            #Whilst we have trailing tactic concatenators
+            while (ntext[-2:] in [r'>-',r'>|',r'>>',r'\\']) or (ntext[-5:] in ['THEN1','THENL']) or (ntext[-4:] == 'THEN'):
+                #get rid of them
+                if ntext[-2:] in [r'>-',r'>|',r'>>',r'\\']:
+                    ntext = ntext[:-2]
+                if ntext[-5:] in ['THEN1','THENL']:
+                    ntext = ntext[:-5]
+                if ntext[-4:] == 'THEN':
+                    ntext = ntext[:-4]
+                #and any revealed whitespace
+                ntext = ntext.rstrip()
+        #OTHER HANDLING
+        else:
+            dL = 0
+            ntext = text
+        #handle if front stuff deleted
+        if dL > 0:
+            #find number of lines and characters deleted
+            deleted_text = text[:dL]
+            deleted_lines = deleted_text.split('\n')
+            nu_lines = len(deleted_lines) - 1
+            nu_chars = len(deleted_lines[-1])
+            #Adjust line number and character number based on deletion
+            if nu_lines > 0:
+                lN += nu_lines
+                cN  = nu_chars
+            else:
+                cN -= nu_chars
+        #construct location string based on line and character number
+        locString = "(*#loc " + str(lN) + " " + str(cN) + " *)"
+        if prepend != "" and ntext == "" and append == "":
+            text = locString + prepend
+        elif prepend != "" or ntext != "" or append != "":
+            text = prepend + locString + ntext + append
         cmd = "hol_repl_" + action
         self.view.window().run_command(cmd, {"external_id": self.repl_external_id(), "text": text})
 
@@ -87,16 +137,23 @@ class HolReplTransferCurrent(sublime_plugin.TextCommand):
     def selected_text(self):
         v = self.view
         parts = [v.substr(region) for region in v.sel()]
-        return "".join(parts)
+        lN,cN = v.rowcol(min([region.a for region in v.sel()]+[region.b for region in v.sel()]))
+        lN += 1
+        return (lN,cN,"".join(parts))
 
     def selected_lines(self):
         v = self.view
         parts = []
+        region_pts = []
         for sel in v.sel():
             for line in v.lines(sel):
                 parts.append(v.substr(line))
-        return "\n".join(parts)
+                region_pts.append(line.a)
+                region_pts.append(line.b)
+        lN,cN = v.rowcol(min(region_pts))
+        lN += 1
+        return (lN,cN,"\n".join(parts))
 
     def selected_file(self):
         v = self.view
-        return v.substr(sublime.Region(0, v.size()))
+        return (1,0,v.substr(sublime.Region(0, v.size())))
